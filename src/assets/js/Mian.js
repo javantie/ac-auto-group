1a651502ca1c8317c9a1a369fab5b9b0c50147e9
Akeem C <javantieclarke@gmail.com>
2021-11-18 12:10:27 -0500

    removed event listener for the nav bar

--- src/assets/js/Mian.js
+++ src/assets/js/Mian.js
@@ -3,6 +3,6 @@ const btn = document.querySelector(".mobile-menu-button");
 const sidebar = document.querySelector(".sidebar");
 
 // add our event listener for the click
-btn.addEventListener("click", () => {
-  sidebar.classList.toggle("-translate-x-full");
-});
+// btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("-translate-x-full");
+// });
