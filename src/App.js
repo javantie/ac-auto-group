681ff7f6829c915b5386e31d7677e5bea0e7be5b
Akeem C <javantieclarke@gmail.com>
2021-11-19 20:32:32 -0500

    updated the nav bar

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import LeadForm from "./components/LeadForm";
 import Testimonials from "./components/Testimonials";
 import ContactPage from "./pages/Contact";
 import Home from "./pages/Home";
+import img_log from "./assets/images/FB_IMG_1575093313783.jpg";
 
 function App() {
   const handleClick = () => {
@@ -28,6 +29,7 @@ function App() {
           <div className=" md:flex">
             {/* <!-- mobile menu bar --> */}
             <div className="bg-green-700 text-gray-100 flex justify-between md:hidden">
+             
               {/* <!-- logo --> */}
               <Link
                 to="/"
@@ -36,6 +38,7 @@ function App() {
                 AC AUTO BUYERS{" "}
                 <i className="fas fa-recycle text-3xl pl-1 text-white animate-spin"></i>
               </Link>
+
               {/* <!-- mobile menu button --> */}
               <button
                 id="mobile-menu"
@@ -49,15 +52,13 @@ function App() {
             {/* <!-- sidebar --> */}
             <aside className="sidebar min-h-screen sm:rounded-none rounded-br-lg bg-green-600 text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
               {/* <!-- logo --> */}
-              <a
-                href="#"
-                className="text-white flex items-center space-x-2 px-4 no-underline"
-              >
-                <i className="fas fa-recycle text-3xl pr-1 animate-spin"></i>
-                <span className="text-2xl font-extrabold font-serif no-underline text-yellow-500">
-                  AC AUTO BUYERS
-                </span>
-              </a>
+              <div className="flex justify-center">
+                <img
+                  src={img_log}
+                  alt="cash-for-junk-car-ac-auto-buyers"
+                  className=" rounded-circle h-32 w-32 border-2 border-yellow-400 p-0.5"
+                />
+              </div>
 
               {/* <!-- nav --> */}
               <nav className="font-bold font-serif text-xl">
@@ -87,36 +88,11 @@ function App() {
                 >
                   How it works
                 </Link>
-                {/* <a
-                  href=""
-                  className="no-underline block py-2.5 px-4 text-white rounded transition duration-200 hover:bg-blue-400"
-                >
-                  About
-                </a> */}
-                {/* <a
-                  href=""
-                  className="no-underline block py-2.5 px-4 text-white rounded transition duration-200 hover:bg-blue-400"
-                >
-                  How It Works
-                </a>
-                <a
-                  href=""
-                  className="no-underline block py-2.5 px-4 text-white rounded transition duration-200 hover:bg-blue-400"
-                >
-                  Contact Us
-                </a> */}
               </nav>
             </aside>
 
             {/* <!-- content --> */}
             <div className="flex-1 md:p-2 p-3">
-              {/* <Hero />
-            <LeadForm />
-            <How />
-            <About />
-            <Testimonials />
-            <ContactForm /> */}
-              {/* <ContactPage /> */}
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about-us" element={<About />} />
