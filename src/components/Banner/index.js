587b23d783246d36aa0a79988c6ad107b4876490
Akeem C <javantieclarke@gmail.com>
2021-11-19 20:42:13 -0500

    updated the nav bar

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -8,8 +8,8 @@ function Banner() {
         <p className="text-xs font-bold text-white sm:text-lg animate-bounce mt-4">We Buy Junk Car - Any Condition!</p>
         <a href="tel:+15183140703" className="text-xs pl-1 text-white font-bold sm:text-lg no-underline border-2 shadow rounded-md p-1 bg-red-600"><i className="fas fa-phone-volume pr-1 text-lg"></i>(518) 314-0703</a>
       </div>
-      <div className="flex justify-center items-center pl-2 w-1/5 animate-pulse pr-2">
-        <a href="https://www.peddle.com/offer?pub_id=289876"  target="_blank"className="bg-yellow-500 text-sm text-center sm:text-lg font-bold no-underline font-serif text-white border-2 rounded-circle p-1"><i className="fas fa-money-bill-alt text-lg text-green-900"></i> INSTANT OFFER</a>
+      <div className="flex justify-center items-center pl-2 w-1/5 animate-pulse pr-2 sm:bg-yellow-500">
+        <a href="https://www.peddle.com/offer?pub_id=289876"  target="_blank"className="bg-yellow-500 text-sm text-center sm:text-lg font-bold no-underline font-serif text-white border-2 sm:border-none sm:rounded-none p-1"><i className="fas fa-money-bill-alt text-lg text-green-900"></i> INSTANT OFFER</a>
       </div>
     </div>
   );
