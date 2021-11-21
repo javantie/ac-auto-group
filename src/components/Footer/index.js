5c11eeafd3e6cb47280726df8f3876a77a4c9260
Akeem C <javantieclarke@gmail.com>
2021-11-21 09:46:28 -0500

    adde google anylitics to track visitors on site

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="border-t-2">
-        <div className="bg-green-800 text-white font-bold text-md">
-            <ul className="flex justify-evenly pr-4">
-                <li>Get Offer</li>
-                <li>About Us</li>
-                <li>Contact US</li>
-                <li>Blog</li>
-            </ul>
-        </div>
+      <div className="bg-green-800">
+        <ul className="flex justify-evenly pr-4">
+          <Link to="/" className="text-white font-bold text-lg sm:text-2xl no-underline">
+            <li>Get Offer</li>
+          </Link>
+          <Link to="about-us"  className="text-white font-bold text-lg sm:text-2xl no-underline">
+            <li>About Us</li>
+          </Link>
+          <Link to="how-it-works" className="text-white font-bold text-lg sm:text-2xl no-underline">
+            <li>FAQ's</li>
+          </Link>
+          <Link to="contact" className="text-white font-bold text-lg sm:text-2xl no-underline">
+            <li>Contact US</li>
+          </Link>
+        </ul>
+      </div>
       <div className="footer__content">
         <div className="text-center">
           <p>
@@ -18,25 +27,25 @@ function Footer() {
               &copy; {new Date().getFullYear()}
             </span>
             <span className="text-xl font-bold font-serif">
-              {' '}
+              {" "}
               AC Auto Buyers
             </span>
           </p>
         </div>
         <div className="flex justify-evenly text-3xl mb-2">
-          <a href="https://www.facebook.com/acautogroup">   
+          <a href="https://www.facebook.com/acautogroup">
             <i className="fab fa-facebook-square "></i>
-            </a>
-            <a href="https://www.instagram.com/acautogroup/">
+          </a>
+          <a href="https://www.instagram.com/acautogroup/">
             <i className="fab fa-instagram"></i>
-            </a>
-            <a href="https://www.linkedin.com/company/ac-auto-group/">
+          </a>
+          <a href="https://www.linkedin.com/company/ac-auto-group/">
             <i className="fab fa-linkedin"></i>
-            </a>
+          </a>
         </div>
-        </div>
+      </div>
     </footer>
-    )
+  );
 }
- 
-export default Footer
+
+export default Footer;
