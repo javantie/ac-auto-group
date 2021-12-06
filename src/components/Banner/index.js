7f9608f31a0be8e53d6ffce195eb97b86b54df13
Akeem C <javantieclarke@gmail.com>
2021-12-06 14:49:41 -0500

    update phone number

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,7 +5,7 @@ function Banner() {
   return (
     <div className="flex sticky top-0 z-0 sm:relative bg-green-900">
       <div className=" flex justify-between pr-2 w-4/5 pt-1 pl-1 items-center mr-0.5">
-        <p className="text-xs font-bold text-white sm:text-lg animate-bounce mt-4">We Buy Junk Car - Any Condition!</p>
+        <p className="text-xs font-bold text-white sm:text-lg animate-bounce mt-4">We Buy Junk Cars - (518)314-0703!</p>
         <a href="tel:+15183140703" className="text-xs pl-1 text-white font-bold sm:text-lg no-underline border-2 shadow rounded-md p-1 mr-1 bg-red-600"><i className="fas fa-phone-volume  text-lg"></i>Call Now</a>
       </div>
       <div className="flex justify-center items-center pl-2 w-1/5 animate-pulse pr-2 sm:bg-yellow-500">
